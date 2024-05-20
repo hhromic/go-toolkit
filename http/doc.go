ff3039ab3028c9a85d4f8158e6609d39ec9c7e19
Hugo Hromic <hhromic@gmail.com>
2024-05-20 20:22:50 +0100

    project: use proper SPDX tag for file copyright

--- http/doc.go
+++ http/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Hugo Hromic
+// SPDX-FileCopyrightText: Copyright 2023 Hugo Hromic
 // SPDX-License-Identifier: Apache-2.0
 
 // The http package provides utilities for net/http use cases.
--- types/doc.go
+++ types/doc.go
@@ -1,4 +1,4 @@
-// Copyright 2023 Hugo Hromic
+// SPDX-FileCopyrightText: Copyright 2023 Hugo Hromic
 // SPDX-License-Identifier: Apache-2.0
 
 // The types package provides useful types for applications.
